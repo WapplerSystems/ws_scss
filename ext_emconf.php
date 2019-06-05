151f35cab980e664891c509fda7d60dacc4b00a5
Sven Wappler <info@wappler.systems>
2019-06-05 20:23:06 +0200

    php dependency corrected

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = [
 	'CGLcompliance_note' => '',
 	'constraints' => [
 		'depends' => [
-            'php' => '7.0.0-7.3.99',
+            'php' => '7.1.0-7.3.99',
 			'typo3' => '8.7.0-9.5.99',
         ],
 		'conflicts' => [
