51307383b848f236aa1818a06c8bd8ae998c32ac
Sven Wappler <info@wappler.systems>
2019-12-26 21:50:10 +0100

    ext_localconf.php updated

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,17 +3,19 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess']['wsscss'] = 'WapplerSystems\\WsScss\\Hooks\\RenderPreProcessorHook->renderPreProcessorProc';
+if (TYPO3_MODE === 'FE') {
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess']['wsscss'] = 'WapplerSystems\\WsScss\\Hooks\\RenderPreProcessorHook->renderPreProcessorProc';
+}
 
 // Caching the pages - default expire 3600 seconds
-if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['ws_scss'])) {
-	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['ws_scss'] = array(
-		'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
-		'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\FileBackend',
+if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['ws_scss'])) {
+	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['ws_scss'] = [
+		'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
+		'backend' => \TYPO3\CMS\Core\Cache\Backend\FileBackend::class,
 		'options' => [
             'defaultLifetime' => 0,
 		]
 
-	);
+    ];
 }
 
