44343413bad5af424641ad78b7585307be880649
David König <david@hausformat.com>
2020-02-11 13:52:42 +0100

    Use $GLOBALS['TYPO3_CONF_VARS'] instead of $TYPO3_CONF_VARS for compatibility with TYPO3 10

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,7 +9,7 @@ if (TYPO3_MODE === 'FE') {
 
 // Caching the pages - default expire 3600 seconds
 if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['ws_scss'])) {
-	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['ws_scss'] = [
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['ws_scss'] = [
 		'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
 		'backend' => \TYPO3\CMS\Core\Cache\Backend\FileBackend::class,
 		'options' => [
