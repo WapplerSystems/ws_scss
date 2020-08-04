dc7144cf946209e6709187ab516e2c37089b2ad9
Sven Wappler <info@wappler.systems>
2020-08-04 13:15:54 +0200

    version set to 1.1.18

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,37 +11,37 @@
  ***************************************************************/
 
 $EM_CONF['ws_scss'] = [
-	'title' => 'SASS compiler for TYPO3',
-	'description' => 'Compiles scss files to CSS files.',
-	'category' => 'fe',
-	'shy' => 0,
-	'version' => '1.1.17',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
-	'author' => 'Sven Wappler',
-	'author_email' => 'typo3YYYY@wappler.systems',
-	'author_company' => 'WapplerSystems',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'constraints' => [
-		'depends' => [
+    'title' => 'SASS compiler for TYPO3',
+    'description' => 'Compiles scss files to CSS files.',
+    'category' => 'fe',
+    'shy' => 0,
+    'version' => '1.1.18',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearcacheonload' => 0,
+    'lockType' => '',
+    'author' => 'Sven Wappler',
+    'author_email' => 'typo3YYYY@wappler.systems',
+    'author_company' => 'WapplerSystems',
+    'CGLcompliance' => '',
+    'CGLcompliance_note' => '',
+    'constraints' => [
+        'depends' => [
             'php' => '7.2.0-7.4.99',
-			'typo3' => '9.5.0-10.4.99',
+            'typo3' => '9.5.0-10.4.99',
         ],
-		'conflicts' => [
+        'conflicts' => [
         ],
-		'suggests' => [
+        'suggests' => [
         ],
     ],
-	'suggests' => [
+    'suggests' => [
     ],
 ];
