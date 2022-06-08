34020beca05b429075a497ebfcaa3ff1e0082169
Marcus Förster <m.foerster@xerc.de>
2022-06-08 19:16:14 +0200

    cleanup EXT base-files

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,37 +1,15 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "ws_scss".
- *
- * Auto generated 07-02-2014 02:04
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF['ws_scss'] = [
     'title' => 'SASS compiler for TYPO3',
     'description' => 'Compiles scss files to CSS files.',
     'category' => 'fe',
-    'shy' => 0,
-    'version' => '1.2.1',
-    'dependencies' => '',
-    'conflicts' => '',
-    'priority' => '',
-    'loadOrder' => '',
-    'module' => '',
     'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'modify_tables' => '',
+    'version' => '1.2.1',
     'clearcacheonload' => 0,
-    'lockType' => '',
     'author' => 'Sven Wappler',
     'author_email' => 'typo3YYYY@wappler.systems',
     'author_company' => 'WapplerSystems',
-    'CGLcompliance' => '',
-    'CGLcompliance_note' => '',
     'constraints' => [
         'depends' => [
             'php' => '7.4.0-8.0.99',
@@ -41,7 +19,5 @@ $EM_CONF['ws_scss'] = [
         ],
         'suggests' => [
         ],
-    ],
-    'suggests' => [
-    ],
+    ]
 ];
