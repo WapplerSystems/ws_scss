5a8778898742306b53d7421087268c7827597c7e
Sven Wappler <info@wappler.systems>
2023-01-16 12:12:44 +0100

    v12 compatibility

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "ws_scss".
+ *
+ * Auto generated 07-02-2014 02:04
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF['ws_scss'] = [
+    'title' => 'SASS compiler for TYPO3',
+    'description' => 'Compiles scss files to CSS files.',
+    'category' => 'fe',
+    'shy' => 0,
+    'version' => '12.0.0',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearcacheonload' => 0,
+    'lockType' => '',
+    'author' => 'Sven Wappler',
+    'author_email' => 'typo3YYYY@wappler.systems',
+    'author_company' => 'WapplerSystems',
+    'CGLcompliance' => '',
+    'CGLcompliance_note' => '',
+    'constraints' => [
+        'depends' => [
+            'php' => '8.0.0-8.2.99',
+            'typo3' => '12.0.0-12.4.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+    'suggests' => [
+    ],
+];
