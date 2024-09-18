3911d51a4960a4cf1a97248949c16dc658c662a1
Sven Wappler <wappler@wappler.systems>
2024-09-18 16:56:39 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,20 +1,10 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "ws_scss".
- *
- * Auto generated 07-02-2014 02:04
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF['ws_scss'] = [
     'title' => 'SASS compiler for TYPO3',
     'description' => 'Compiles scss files to CSS files.',
     'category' => 'fe',
-    'version' => '12.0.5',
+    'version' => '13.0.0',
     'state' => 'stable',
     'clearcacheonload' => 0,
     'author' => 'Sven Wappler',
@@ -22,8 +12,8 @@ $EM_CONF['ws_scss'] = [
     'author_company' => 'WapplerSystems',
     'constraints' => [
         'depends' => [
-            'php' => '8.0.0-8.2.99',
-            'typo3' => '12.0.0-12.4.99',
+            'php' => '8.0.0-8.3.99',
+            'typo3' => '13.0.0-13.4.99',
         ],
         'conflicts' => [
         ],
