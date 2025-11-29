9c9e31df53a3242615302822ddbb2e00b9d7b2a6
Sven Wappler <wappler@wappler.systems>
2025-11-29 03:23:37 +0100

    TYPO3 14 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF['ws_scss'] = [
     'title' => 'SASS compiler for TYPO3',
     'description' => 'Compiles scss files to CSS files.',
     'category' => 'fe',
-    'version' => '13.1.6',
+    'version' => '14.0.0',
     'state' => 'stable',
     'clearcacheonload' => 0,
     'author' => 'Sven Wappler',
@@ -12,8 +12,8 @@ $EM_CONF['ws_scss'] = [
     'author_company' => 'WapplerSystems',
     'constraints' => [
         'depends' => [
-            'php' => '8.0.0-8.4.99',
-            'typo3' => '13.0.0-13.4.99',
+            'php' => '8.2.0-8.4.99',
+            'typo3' => '14.0.0',
         ],
         'conflicts' => [
         ],
