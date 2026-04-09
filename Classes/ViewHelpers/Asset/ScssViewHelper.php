2e9d514c4e6ff11aa9a3075e6b2b58e57c38377a
Sven Wappler <wappler@wappler.systems>
2026-04-09 22:39:12 +0200

    Fix ScssViewHelper for TYPO3 v14 / Fluid v4: constructor DI, typed addAttribute signature

--- Classes/ViewHelpers/Asset/ScssViewHelper.php
+++ Classes/ViewHelpers/Asset/ScssViewHelper.php
@@ -1,8 +1,7 @@
 <?php
-declare (strict_types=1);
+declare(strict_types=1);
 
 namespace WapplerSystems\WsScss\ViewHelpers\Asset;
-
 
 use ScssPhp\ScssPhp\Exception\SassException;
 use TYPO3\CMS\Core\Cache\Exception\NoSuchCacheException;
@@ -17,34 +16,21 @@ use WapplerSystems\WsScss\Compiler;
 
 class ScssViewHelper extends AbstractTagBasedViewHelper
 {
-    /**
-     * This VH does not produce direct output, thus does not need to be wrapped in an escaping node
-     *
-     * @var bool
-     */
     protected $escapeOutput = false;
-
-    /**
-     * Rendered children string is passed as CSS code,
-     * there is no point in HTML encoding anything from that.
-     *
-     * @var bool
-     */
     protected $escapeChildren = true;
 
-    protected AssetCollector $assetCollector;
-
-    public function injectAssetCollector(AssetCollector $assetCollector): void
-    {
-        $this->assetCollector = $assetCollector;
+    public function __construct(
+        protected readonly AssetCollector $assetCollector,
+    ) {
+        parent::__construct();
     }
 
     public function initialize(): void
     {
-        // Add a tag builder, that does not html encode values, because rendering with encoding happens in AssetRenderer
+        // Add a tag builder that does not html encode values, because rendering with encoding happens in AssetRenderer
         $this->setTagBuilder(
             new class () extends TagBuilder {
-                public function addAttribute($attributeName, $attributeValue, $escapeSpecialCharacters = false): void
+                public function addAttribute(string $attributeName, $attributeValue, bool $escapeSpecialCharacters = false): void
                 {
                     parent::addAttribute($attributeName, $attributeValue, false);
                 }
@@ -70,27 +56,14 @@ class ScssViewHelper extends AbstractTagBasedViewHelper
         $this->registerTagAttribute('sizes', 'string', 'Define the icon size of the resource.', false);
         $this->registerTagAttribute('type', 'string', 'Define the MIME type (usually \'text/css\').', false);
         $this->registerTagAttribute('nonce', 'string', 'Define a cryptographic nonce (number used once) used to whitelist inline styles in a style-src Content-Security-Policy.', false);
-        $this->registerArgument(
-            'identifier',
-            'string',
-            'Use this identifier within templates to only inject your CSS once, even though it is added multiple times.',
-            true
-        );
-        $this->registerArgument(
-            'priority',
-            'boolean',
-            'Define whether the CSS should be included before other CSS. CSS will always be output in the <head> tag.',
-            false,
-            false
-        );
+        $this->registerArgument('identifier', 'string', 'Use this identifier within templates to only inject your CSS once, even though it is added multiple times.', true);
+        $this->registerArgument('priority', 'boolean', 'Define whether the CSS should be included before other CSS. CSS will always be output in the <head> tag.', false, false);
         $this->registerArgument('scssVariables', 'mixed', 'An optional array of variables to be set inside the SCSS context', false);
         $this->registerArgument('outputfile', 'string', '', false);
         $this->registerArgument('forcedOutputLocation', 'string', 'force "inline" or "file"', false, '');
     }
 
-
     /**
-     * @return string
      * @throws FileDoesNotExistException
      * @throws SassException
      * @throws NoSuchCacheException
@@ -98,14 +71,12 @@ class ScssViewHelper extends AbstractTagBasedViewHelper
      */
     public function render(): string
     {
-
         $identifier = (string)$this->arguments['identifier'];
         $attributes = $this->tag->getAttributes();
         $variables = (array)$this->arguments['scssVariables'];
         $outputFile = $this->arguments['outputfile'];
         $forcedOutputLocation = $this->arguments['forcedOutputLocation'];
 
-        // boolean attributes shall output attr="attr" if set
         if ($attributes['disabled'] ?? false) {
             $attributes['disabled'] = 'disabled';
         }
@@ -114,22 +85,21 @@ class ScssViewHelper extends AbstractTagBasedViewHelper
         unset($attributes['href']);
 
         $options = [
-            'priority' => $this->arguments['priority']
+            'priority' => $this->arguments['priority'],
         ];
 
         $variables = $this->cleanVariables($variables);
 
         if ($file !== null) {
-
             $scssFilePath = GeneralUtility::getFileAbsFileName($file);
             if ($scssFilePath === '') {
-                throw new ContentRenderingException('Could not resolve the path to the SCSS file. Probably the path is not correct! Path: '.$file);
+                throw new ContentRenderingException('Could not resolve the path to the SCSS file. Probably the path is not correct! Path: ' . $file);
             }
-            $pathChunks = explode('/',PathUtility::getAbsoluteWebPath($scssFilePath));
+            $pathChunks = explode('/', PathUtility::getAbsoluteWebPath($scssFilePath));
             if (self::usesComposerClassLoading()) {
-                $assetPath = implode('/',array_splice($pathChunks,0,3)).'/';
+                $assetPath = implode('/', array_splice($pathChunks, 0, 3)) . '/';
             } else {
-                $assetPath = implode('/',array_splice($pathChunks,0,6)).'/';
+                $assetPath = implode('/', array_splice($pathChunks, 0, 6)) . '/';
             }
             $variables['extAssetPath'] = $assetPath;
 
@@ -141,7 +111,6 @@ class ScssViewHelper extends AbstractTagBasedViewHelper
             } else {
                 $this->assetCollector->addStyleSheet($identifier, $cssFile, $attributes, $options);
             }
-
         } else {
             $content = (string)$this->renderChildren();
             $cssFile = Compiler::compileSassString($content, $variables, $outputFile);
@@ -152,12 +121,11 @@ class ScssViewHelper extends AbstractTagBasedViewHelper
                 $content = file_get_contents($cssFile);
                 $this->assetCollector->addInlineStyleSheet($identifier, $content, $attributes, $options);
             }
-
         }
         return '';
     }
 
-    private function cleanVariables($variables): array
+    private function cleanVariables(array $variables): array
     {
         foreach ($variables as $key => $value) {
             if (is_array($value)) {
@@ -174,5 +142,4 @@ class ScssViewHelper extends AbstractTagBasedViewHelper
     {
         return defined('TYPO3_COMPOSER_MODE') && TYPO3_COMPOSER_MODE;
     }
-
 }
